089ca8ed42d7bb6e40d58a93134efea80afaee44
alikskonst <alikskonst@gmail.com>
2022-06-28 16:29:03 +0300

    commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,7 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import web.model.Car;
 
 import java.util.ArrayList;
@@ -29,9 +29,9 @@ public class CarController {
         return "cars";
     }
 
-//    @GetMapping(value = "/cars")
-//    public String cars2(ModelMap model, @RequestParam int count) {
-//        model.addAttribute("carList", count >= carList.size() ? carList : carList.subList(0, count));
-//        return "cars";
-//    }
+    @GetMapping(value = "/cars?count={count}")
+    public String cars2(ModelMap model, @PathVariable int count) {
+        model.addAttribute("carList", count >= carList.size() ? carList : carList.subList(0, count));
+        return "cars";
+    }
 }
